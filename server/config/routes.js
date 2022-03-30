6bcee162ad36bae2426ee3246e08051a9d204958
HP-ELITEBOOK\Developer <n.nikolaevbg97@gmail.com>
2022-03-30 22:42:34 +0300

    Working on front end bootstrap. Fixed form pages for login, register and website create. Finishing navbar.

--- server/config/routes.js
+++ server/config/routes.js
@@ -3,6 +3,7 @@ const { VIEWS } = require('../utilities/constants');
 
 module.exports = app => {
     app.get('/', controllers.home.index);
+    app.get('/about', controllers.home.about);
 
     app.use(controllers.articles);
     app.use(controllers.users);
--- server/controllers/home.js
+++ server/controllers/home.js
@@ -1,4 +1,7 @@
 module.exports = {
+    about: (req, res) => {
+        res.render('home/about');
+    },
     index: (req, res) => {
         res.render('home/index');
     }
